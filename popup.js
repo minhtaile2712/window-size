caf82f68abad59d280274163b3b328ab9cd6b734
minhtaile2712 <minhtaile2712@gmail.com>
2025-06-24 10:17:53 +0700

    Update options

--- popup.js
+++ popup.js
@@ -9,18 +9,15 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
 
 const windowSizes = {
   "800x960": { width: 800, height: 960 },
-  "960x960": { width: 960, height: 960 },
-  "1280x720": { width: 1280, height: 720 },
+  "960x600": { width: 960, height: 600 },
   "1280x800": { width: 1280, height: 800 },
-  "1280x960": { width: 1280, height: 960 },
-  "1280x1024": { width: 1280, height: 1024 },
-  "1920x1080": { width: 1920, height: 1080 },
-  "1920x1200": { width: 1920, height: 1200 },
+  "1440x900": { width: 1440, height: 900 },
+  "1536x960": { width: 1536, height: 960 },
+  // "1920x1080": { width: 1920, height: 1080 },
+  // "1920x1200": { width: 1920, height: 1200 },
 };
 
 document.getElementById("size-select").addEventListener("change", (event) => {
-  if (event.target.value == "current") return;
-
   chrome.windows.getCurrent((window) => {
     chrome.windows.update(window.id, {
       width: windowSizes[event.target.value].width + offset,
