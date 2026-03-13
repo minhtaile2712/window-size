0a3f9ea6880d7e5fa8279c9e0151f3d28a1c15fe
minhtaile2712 <minhtaile2712@gmail.com>
2026-03-14 06:17:07 +0700

    rename variable

--- background.js
+++ background.js
@@ -1,20 +1,20 @@
-chrome.windows.onBoundsChanged.addListener(async (window) => {
-  const [tab] = await chrome.tabs.query({ windowId: window.id, active: true });
-  await updateBadge(tab.id, tab.url, window);
+chrome.windows.onBoundsChanged.addListener(async (win) => {
+  const [tab] = await chrome.tabs.query({ windowId: win.id, active: true });
+  await updateBadge(tab.id, tab.url, win);
 });
 
 chrome.tabs.onActivated.addListener(async ({ tabId, windowId }) => {
-  const [tab, window] = await Promise.all([chrome.tabs.get(tabId), chrome.windows.get(windowId)]);
-  await updateBadge(tab.id, tab.url, window);
+  const [tab, win] = await Promise.all([chrome.tabs.get(tabId), chrome.windows.get(windowId)]);
+  await updateBadge(tab.id, tab.url, win);
 });
 
-async function updateBadge(tabId, tabUrl, window) {
+async function updateBadge(tabId, tabUrl, win) {
   const { os } = await chrome.runtime.getPlatformInfo();
   const offset = os === "win" ? 16 : 32; // "win", "mac", "linux", "android", "cros", "openbsd"
   let width;
   if (!tabUrl || !tabUrl.startsWith("http")) {
-    width = window.width;
-    if (window.state !== "maximized" && window.state !== "fullscreen") width -= offset;
+    width = win.width;
+    if (win.state !== "maximized" && win.state !== "fullscreen") width -= offset;
   } else {
     const [{ result }] = await chrome.scripting.executeScript({ target: { tabId }, func: () => window.innerWidth });
     width = result;
